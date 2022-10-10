355784a8289fc9120762005c4d23f7d636efbbbc
ChiaShengTu <chiashengtu@utexas.edu>
2022-10-10 15:25:02 -0500

    assist_plot

---
+++ code/assist_graph.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+IN_PATH = os.path.join("artifacts", "results_integrated.csv")
+OUT_PATH = os.path.join("plot", "assist.png")
+
+df = pd.read_csv(IN_PATH)
+
+fig = plt.figure(figsize = (11,11))
+ax = fig.add_subplot(1, 1, 1)
+plt.scatter(df['assist'], df['name'])
+plt.title('Age')
+plt.xlabel('assist')
+plt.ylabel('name') 
+plt.savefig(os.path.join(OUT_PATH))
+plt.show()
+
