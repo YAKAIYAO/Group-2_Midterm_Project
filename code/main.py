8bb58634315a57a8159cdf15acc3987e6f48d029
whalesofheaven <78710655+whalesofheaven@users.noreply.github.com>
2022-10-10 15:38:11 -0500

    Merge results_player.csv and results_ranking.csv

--- code/main.py
+++ code/main.py
@@ -7,3 +7,10 @@ from scrape_player import exe_scrape_player
 exe_scrape_player()
 # Do the scarpe_ranking.py and make the results_player.csv
 exe_scrape_player()
+
+# Merge results_player.csv and results_ranking.csv
+# 1. Basic Setup
+BASE_DIR = "artifacts"
+Ranking_CSV_PATH = os.path.join(BASE_DIR, "results_ranking.csv")
+Player_CSV_PATH = os.path.join(BASE_DIR, "results_player.csv")
+Integrated_CSV_PATH = os.path.join(BASE_DIR, "results_integrated.csv")
