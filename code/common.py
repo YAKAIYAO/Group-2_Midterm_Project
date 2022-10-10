68dfe2fd7e2b6cfcfd2c2a4f08c61b053084828a
whalesofheaven <78710655+whalesofheaven@users.noreply.github.com>
2022-10-10 15:24:47 -0500

    get soup fn finished

--- code/common.py
+++ code/common.py
@@ -1,2 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+
+def get_soup(url):
+
+    response = requests.get(url)
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
