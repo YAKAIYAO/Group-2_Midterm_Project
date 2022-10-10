818cab3faa76a788e053ebbc17c206cc8b4278f2
Hasutake <78459812+THashimoto27@users.noreply.github.com>
2022-10-10 15:18:44 -0500

    Take issue 2 : scrape_player_pages2

--- code/scrape_player_pages.py
+++ code/scrape_player_pages.py
@@ -15,3 +15,38 @@ def scrape_page(current_URL):
     soup = get_soup(current_URL)
     player_scraping(soup, id)
     return data
+    
+
+def player_scraping(soup,id):
+    # scrpaing data
+    id = int(id)
+    # if error, igonore this player
+    try: 
+        name = soup.title.text.replace(" Stats, News, Bio | ESPN","")
+        position = soup.find_all("div", class_="PlayerHeader__Team n8 mt3 mb4 flex items-center mt3 mb4 clr-gray-01")[0].find_all("li", class_="")[1].text    
+        height = soup.find_all("div", class_="fw-medium clr-black")[0].text.split(",")[0]
+        weight = int(soup.find_all("div", class_="fw-medium clr-black")[0].text.split(",")[1].replace(" ","").replace("lbs",""))
+        age = int(soup.find_all("ul", class_="PlayerHeader__Bio_List flex flex-column list clr-gray-04")[0].find_all("div", class_="fw-medium clr-black")[1].text.split(" ")[1].replace("(","").replace(")",""))
+        assist = float(soup.find_all("ul", class_="StatBlock__Content flex list ph4 pv3 justify-between")[0].find_all("div",class_="StatBlockInner__Value tc fw-medium n2 clr-gray-02")[2].text)
+        pts = float(soup.find_all("ul", class_="StatBlock__Content flex list ph4 pv3 justify-between")[0].find_all("div",class_="StatBlockInner__Value tc fw-medium n2 clr-gray-02")[0].text)
+
+        height = feet_change(height)
+    except:
+        name=""
+        position=""
+        height=""
+        weight=""
+        age=""
+        assist=""
+        pts=""
+    data.loc[id] = [id,name,position,height,weight,age,assist,pts]
+    return
+
+def feet_change(feet):
+    # 6' 2'' -> 6.2feet
+    changed_feet = float(feet.split(" ")[0].replace("'",""))+1/100*float(feet.split(" ")[1][:-1])
+    return changed_feet
+
+if __name__ == "__main__":
+    url = "https://www.espn.com/nba/player/_/id/4238/eric-bledsoe"
+    scrape_page(url)
