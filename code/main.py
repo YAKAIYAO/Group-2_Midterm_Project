1f5d8219a8130796f3ac5e219e54988394ca5622
whalesofheaven <78710655+whalesofheaven@users.noreply.github.com>
2022-10-10 15:35:58 -0500

    created main.py

---
+++ code/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import os
+from scrape_ranking import exe_scrape_ranking
+from scrape_player import exe_scrape_player
+
+# Do the scarpe_ranking.py and make the results_ranking.csv
+exe_scrape_player()
+# Do the scarpe_ranking.py and make the results_player.csv
+exe_scrape_player()
